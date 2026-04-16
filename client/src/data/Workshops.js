574bc996b2456352f646a6abb56faa488f7fea97
Antonio Lucena <lucenacalderon90@outlook.es>
2026-04-16 16:36:20 +0200

    act

--- client/src/data/Workshops.js
+++ client/src/data/Workshops.js
@@ -15,7 +15,7 @@ export const workshops = [
     description:'El sueño infantil no es igual para todos los bebés, porque ningún sistema nervioso es igual. En este curso aprenderás a leer el perfil sensorial único de tu hijo y a crear un entorno y una rutina que funcionen para él. Un enfoque basado en Integración Sensorial y neurodesarrollo, diseñado para acompañar sin forzar.',
     city:'Clínica de Fisioterapia X-librio, Montilla (Córdoba)',
     duration:'3 horas',
-    workshop_start_date: '11-04-2026',
+    workshop_start_date: '23-05-2026',
     workshop_end_date: '',
     price: '50€'
   },

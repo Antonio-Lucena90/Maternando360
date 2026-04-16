a7a298f71dbac5f97c56464fc01922a19b01e9d9
Antonio Lucena <lucenacalderon90@outlook.es>
2026-04-16 17:06:05 +0200

    act

--- client/src/data/Workshops.js
+++ client/src/data/Workshops.js
@@ -9,16 +9,7 @@ export const workshops = [
     workshop_end_date: '',
     price: '50€'
   }, */ 
-  {
-    id: 2,
-    workshop_name: 'Taller de Sueño, Descanso y regulación. Desde el enfoque de la Integración Sensorial. 1ª edición.',
-    description:'El sueño infantil no es igual para todos los bebés, porque ningún sistema nervioso es igual. En este curso aprenderás a leer el perfil sensorial único de tu hijo y a crear un entorno y una rutina que funcionen para él. Un enfoque basado en Integración Sensorial y neurodesarrollo, diseñado para acompañar sin forzar.',
-    city:'Clínica de Fisioterapia X-librio, Montilla (Córdoba)',
-    duration:'3 horas',
-    workshop_start_date: '23-05-2026',
-    workshop_end_date: '',
-    price: '50€'
-  },
+
   {
     id: 3,
     workshop_name: 'Taller de Sueño, Descanso y regulación. Desde el enfoque de la Integración Sensorial. 1ª edición.',
@@ -28,6 +19,17 @@ export const workshops = [
     workshop_start_date: '25-04-2026',
     workshop_end_date: '',
     price: '50€'
-  }
+  },
+
+    {
+    id: 2,
+    workshop_name: 'Taller de Sueño, Descanso y regulación. Desde el enfoque de la Integración Sensorial. 1ª edición.',
+    description:'El sueño infantil no es igual para todos los bebés, porque ningún sistema nervioso es igual. En este curso aprenderás a leer el perfil sensorial único de tu hijo y a crear un entorno y una rutina que funcionen para él. Un enfoque basado en Integración Sensorial y neurodesarrollo, diseñado para acompañar sin forzar.',
+    city:'Clínica de Fisioterapia X-librio, Montilla (Córdoba)',
+    duration:'3 horas',
+    workshop_start_date: '23-05-2026',
+    workshop_end_date: '',
+    price: '50€'
+  },
  
 ]

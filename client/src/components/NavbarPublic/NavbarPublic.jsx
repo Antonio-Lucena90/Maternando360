285b36855eb587f057c35fa2f265e246928a33b7
Antonio Lucena <lucenacalderon90@outlook.es>
2026-05-17 23:08:26 +0200

    arreglo

--- client/src/components/NavbarPublic/NavbarPublic.jsx
+++ client/src/components/NavbarPublic/NavbarPublic.jsx
@@ -5,53 +5,44 @@ import './NavbarPublic.css';
 import logo from '../../assets/images/logonegro.png'
 
 export const NavbarPublic = () => {
- const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   return (
     <Navbar expand="lg" className="navbar">
-  <Container>
-    <Navbar.Brand as={Link} to="/">
-      <img src={logo} alt="Maternando360 logo" />
-    </Navbar.Brand>
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} alt="Maternando360 logo" />
+        </Navbar.Brand>
 
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto text-center text-lg-start">
-        <Nav.Link as={NavLink} to="/">Inicio</Nav.Link>
-        <Nav.Link as={NavLink} to="/about">Sobre mí</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto text-center text-lg-start">
+            <Nav.Link as={NavLink} to="/">Inicio</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">Sobre mí</Nav.Link>
 
-        <NavDropdown title="Servicios" id="servicios-dropdown">
-          <NavDropdown.Item as={NavLink} to="/accompaniment">
-            Acompañamiento
-          </NavDropdown.Item>
-          <NavDropdown.Item as={NavLink} to="/dream">
-            Regulación y Sueño
-          </NavDropdown.Item>
-        </NavDropdown>
-{/* 
-        <NavDropdown title="Formación" id="formacion-dropdown">
-          <NavDropdown.Item as={NavLink} to="/workshops">
-            Talleres
-          </NavDropdown.Item>
-          <NavDropdown.Item as={NavLink} to="/courses">
-            Cursos
-          </NavDropdown.Item>
-        </NavDropdown> */}
+            <NavDropdown title="Servicios" id="servicios-dropdown">
+              <NavDropdown.Item as={NavLink} to="/accompaniment">
+                Acompañamiento
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/dream">
+                Regulación y Sueño
+              </NavDropdown.Item>
+            </NavDropdown>
 
-        <Nav.Link as={NavLink} to="/rates">Asesoramiento gratuito</Nav.Link>
-      </Nav>
+            <Nav.Link as={NavLink} to="/rates">Asesoramiento gratuito</Nav.Link>
+          </Nav>
 
-    <div className="d-flex flex-column flex-lg-row gap-2 mt-3 mt-lg-0">
-        <Button className="my-btn" onClick={() => navigate('/login')}>
-          Iniciar Sesión
-        </Button>
-        <Button className="my-btn" onClick={() => navigate('/register')}>
-          Registro
-        </Button>
-      </div>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+          <div className="d-flex flex-column flex-lg-row gap-2 mt-3 mt-lg-0">
+            <Button className="my-btn" onClick={() => navigate('/login')}>
+              Iniciar Sesión
+            </Button>
+            <Button className="my-btn" onClick={() => navigate('/register')}>
+              Registro
+            </Button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };

e7a6c48d9fa89f1e9d18ce61e49799adc1b6363c
Antonio Lucena <lucenacalderon90@outlook.es>
2026-05-17 23:46:47 +0200

    Migrar workshops.dal.js de MySQL a PostgreSQL

--- server/modules/workshops/workshops.dal.js
+++ server/modules/workshops/workshops.dal.js
@@ -1,44 +1,45 @@
-  import executeQuery from "../../config/db.js";
+import executeQuery from "../../config/db.js";
 
-  class workshopDal {
+class workshopDal {
 
-    createWorkshop = async(values) => {
+  createWorkshop = async(values) => {
     try {
-      let sql = 'INSERT INTO workshop (workshop_name, description, city, duration, workshop_start_date, workshop_end_date) VALUES (?,?,?,?,?,?)'
+      let sql = 'INSERT INTO workshop (workshop_name, description, city, duration, workshop_start_date, workshop_end_date) VALUES ($1,$2,$3,$4,$5,$6)'
       let result = await executeQuery(sql, values);
-      } catch (error) {
-      throw error
-      }
-    }
-
-    allWorkshops = async() => {
-      try {
-        let sql = 'SELECT * FROM workshop'
-        let result = await executeQuery(sql);
-        return result;
-      } catch (error) {
-        throw error;
-      }
-    }
-
-    editWorkshop = async(values) => {
-      try {
-        let sql = 'UPDATE workshop SET  workshop_name = ?, description = ?, city = ?, duration = ? ,workshop_start_date = ?, workshop_end_date = ? WHERE workshop_id = ?'
-        let result = await executeQuery(sql, values); 
-        return result;
-      } catch (error) {
-        throw error;
-      } 
-    }
-
-    deleteWorkshop = async(values) => {
-      try {
-        let sql = 'DELETE FROM workshop WHERE workshop_id = ?';
-        await executeQuery(sql,values);
-      } catch (error) {
-        throw error; 
-      }
+      return result;
+    } catch (error) {
+      throw error;
     }
   }
-  
- export default new workshopDal();
+
+  allWorkshops = async() => {
+    try {
+      let sql = 'SELECT * FROM workshop';
+      let result = await executeQuery(sql);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  editWorkshop = async(values) => {
+    try {
+      let sql = 'UPDATE workshop SET workshop_name=$1, description=$2, city=$3, duration=$4, workshop_start_date=$5, workshop_end_date=$6 WHERE workshop_id=$7'
+      let result = await executeQuery(sql, values);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  deleteWorkshop = async(values) => {
+    try {
+      let sql = 'DELETE FROM workshop WHERE workshop_id=$1';
+      await executeQuery(sql, values);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default new workshopDal();
